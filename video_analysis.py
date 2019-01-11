20d421fce1abf9261b78d09ac6d896bb4ae25ba8
Timur Gladkikh <tgladkikh@freshbooks.com>
2019-01-11 15:28:39 -0500

    Added video analysis script

--- video_analysis.py
+++ video_analysis.py
@@ -2,15 +2,14 @@ import argparse
 import boto3
 
 
-def extractText(source, destination):
+def extractTextRekognition(source, destination, object_name):
     client = boto3.client('rekognition')
 
-    name = 'frame24.jpg'
-    response = client.detect_text(Image={'S3Object': {'Bucket': source, 'Name': name}})
-    textDetection = response['TextDetections']
+    response = client.detect_text(Image={'S3Object': {'Bucket': source, 'Name': object_name}})
+    text_detection = response['TextDetections']
     print(response)
     print('Detected text')
-    for text in textDetection:
+    for text in text_detection:
         print('Detected text: ' + text['DetectedText'])
         print('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
         print('Id: {}'.format(text['Id']))
@@ -19,14 +18,14 @@ def extractText(source, destination):
         print('Type:' + text['Type'])
 
 
-
 def main():
     a = argparse.ArgumentParser()
     a.add_argument('--source', required=True, help='S3 bucket containing source image files')
     a.add_argument('--dest', required=True, help='Destination path for Rekognition result')
+    a.add_argument('--object', required=True, help='Name of the object in S3 bucket that requires text extraction')
     args = a.parse_args()
     print(args)
-    extractText(args.source, args.dest)
+    extractTextRekognition(args.source, args.dest, args.object)
 
 
 if __name__ == '__main__':
